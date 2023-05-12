e125b404b8dad324c123beb48eaf8c8f8a1e5392
Alex Pasichnichenko <opasichnichenko@digitalsamba.com>
2023-05-12 12:39:21 +0300

    fix: add dsEmbedFrame search param to controlled frame to detect SDK sessions

--- dist/cjs/index.js
+++ dist/cjs/index.js
@@ -200,9 +200,12 @@ class DigitalSambaEmbedded extends events_1.default {
             if (team && room) {
                 url = `https://${team}.digitalsamba.com/${room}`;
             }
-            if (url && token) {
+            if (url) {
                 const urlObj = new URL(url);
-                urlObj.searchParams.append('token', token);
+                urlObj.searchParams.append('dsEmbedFrame', 'true');
+                if (token) {
+                    urlObj.searchParams.append('token', token);
+                }
                 url = urlObj.toString();
             }
             if (url) {
@@ -214,7 +217,10 @@ class DigitalSambaEmbedded extends events_1.default {
             }
             const allowedURL = new URL(this.frame.src);
             this.allowedOrigin = allowedURL.origin;
-            this.frame.onload = () => this.checkTarget();
+            this.frame.onload = () => {
+                this._emit('frameLoaded');
+                this.checkTarget();
+            };
         };
         this.logError = (error) => {
             if (this.reportErrors) {
