9866ecd59f1c1fa09ac95c9714ca4d6593375edc
Alex Pasichnichenko <opasichnichenko@digitalsamba.com>
2024-10-07 14:49:05 +0300

    modify enumerateDevices so media streams dont stay active

--- src/utils/enumerateDevices.ts
+++ src/utils/enumerateDevices.ts
@@ -4,15 +4,14 @@ export const enumerateDevices = async () => {
   let devices: MediaDeviceInfo[] = [];
 
   if (isFirefox()) {
-    // getUserMedia call for FF;
-    await navigator.mediaDevices
-      .getUserMedia({ audio: true, video: true })
-      .then(async () => {
-        devices = await navigator.mediaDevices.enumerateDevices();
-      })
-      .catch(async () => {
-        devices = await navigator.mediaDevices.enumerateDevices();
-      });
+    try {
+      const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+      devices = await navigator.mediaDevices.enumerateDevices();
+
+      stream.getTracks().forEach((track) => track.stop());
+    } catch (err) {
+      devices = await navigator.mediaDevices.enumerateDevices();
+    }
   } else {
     devices = await navigator.mediaDevices.enumerateDevices();
   }
