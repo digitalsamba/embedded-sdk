48058f61a85a5f8095a4d8bf7eabd1b4be57ad02
YarmolenkoDaniel <dyarmolenko@digitalsamba.com>
2024-12-27 14:54:38 +0200

    feat: add new createWhiteboard method, and update exist whiteboard methods

--- src/types.ts
+++ src/types.ts
@@ -160,6 +160,7 @@ export type SendMessageType =
   | 'changeRole'
   | 'addTileAction'
   | 'removeTileAction'
+  | 'createWhiteboard'
   | 'openWhiteboard'
   | 'closeWhiteboard'
   | 'toggleWhiteboard'
@@ -210,7 +211,7 @@ export const receiveMessagesTypes = [
   'userLeftBatch',
 ] as const;
 
-export type ReceiveMessageType = (typeof receiveMessagesTypes)[number];
+export type ReceiveMessageType = typeof receiveMessagesTypes[number];
 
 export type UICallbackName = 'leaveSession';
 
@@ -367,7 +368,12 @@ export type AddImageToWhiteboardOptions = {
   position?: {
     x: number;
     y: number;
-  }
+  };
+};
+
+export type CreateWhiteboardOptions = {
+  personal: boolean;
+  folderId: string;
 };
 
 export interface EmbeddedInstance {
@@ -395,9 +401,10 @@ export interface EmbeddedInstance {
   disableAudio: () => void;
   toggleAudio: (enable?: boolean) => void;
   addImageToWhiteboard: (options: AddImageToWhiteboardOptions) => void;
-  openWhiteboard: () => void;
-  closeWhiteboard: () => void;
-  toggleWhiteboard: (show?: boolean) => void;
+  createWhiteboard: (options: CreateWhiteboardOptions) => void;
+  openWhiteboard: (id?: string) => void;
+  closeWhiteboard: (id?: string) => void;
+  toggleWhiteboard: (show?: boolean, id?: string) => void;
   startScreenshare: () => void;
   stopScreenshare: () => void;
   startRecording: () => void;
