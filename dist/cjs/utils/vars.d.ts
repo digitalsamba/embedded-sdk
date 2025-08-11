a0f0a1175d2331588f38fe82805e388c3d4266bf
Daniil Yarmolenko <daniil@Daniils-MacBook-Pro.local>
2025-08-11 11:46:51 +0300

    bump version to 0.0.49

--- dist/cjs/utils/vars.d.ts
+++ dist/cjs/utils/vars.d.ts
@@ -1,5 +1,5 @@
 import { Stored } from '../types';
-export declare const PACKAGE_VERSION = "0.0.48";
+export declare const PACKAGE_VERSION = "0.0.49";
 export declare const CONNECT_TIMEOUT = 10000;
 export declare const internalEvents: Record<string, boolean>;
 export declare enum LayoutMode {
